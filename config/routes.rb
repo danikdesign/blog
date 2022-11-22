06f25dd7535f7efd6ffaf4d48453bc1f849f29fe
danikdesign <danik.newmail@gmail.com>
2022-11-22 14:37:34 +0200

    Added /about controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles
   resource :terms, only: [:show]
+  resource :about, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
