a8a45b849360696c85a9d4bbf6d83890e4b2d2ba
danikdesign <danik.newmail@gmail.com>
2022-11-25 16:43:03 +0200

    Deleted unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles do
-    resources :comments
+    resources :comments, only: [:new, :create]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
