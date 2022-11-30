a64ffaffc4b00ec981e665c82f1b6ef1232ff161
danikdesign <danik.newmail@gmail.com>
2022-11-30 14:22:42 +0200

    Permit username for signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_in) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
+  end
 end
