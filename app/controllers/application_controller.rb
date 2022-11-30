85a87652a255d01c9ac3e7259a1e2114d662729e
danikdesign <danik.newmail@gmail.com>
2022-11-30 14:38:12 +0200

    Added username field at signup view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:sign_in) << :username
-    devise_parameter_sanitizer.for(:account_update) << :username
   end
 end
