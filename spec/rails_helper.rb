1607a78bb5aae47a0c3110b69cc4a5ca00071e39
danikdesign <danik.newmail@gmail.com>
2022-12-10 22:34:32 +0200

    Refactor: moved sign_up method to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,3 +72,15 @@ end
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
+
+#session_helper
+def sign_up
+  visit new_user_registration_path
+
+  fill_in :user_email, with: 'user@example.com'
+  fill_in :user_username, with: 'John'
+  fill_in :user_password, with: 'secure123#!'
+  fill_in :user_password_confirmation, with: 'secure123#!'
+
+  click_button 'Sign up'
+end
