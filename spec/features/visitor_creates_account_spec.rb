0160a0e16a001377b02bb99f1a4b0339fb863558
danikdesign <danik.newmail@gmail.com>
2022-12-10 22:29:27 +0200

    Created sign_up method

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 feature "Account Creation" do
   scenario "allow guest to create account" do
-    visit new_user_registration_path
-
-    fill_in :user_email, with: 'user@example.com'
-    fill_in :user_username, with: 'John'
-    fill_in :user_password, with: 'secure123#!'
-    fill_in :user_password_confirmation, with: 'secure123#!'
-
-    click_button 'Sign up'
+    sign_up
 
     expect(page).to have_content 'New'
 
   end
+end
+
+def sign_up
+  visit new_user_registration_path
+
+  fill_in :user_email, with: 'user@example.com'
+  fill_in :user_username, with: 'John'
+  fill_in :user_password, with: 'secure123#!'
+  fill_in :user_password_confirmation, with: 'secure123#!'
+
+  click_button 'Sign up'
 end
