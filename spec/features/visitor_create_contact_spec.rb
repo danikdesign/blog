52a531faa86bb86df326633ce0432062b6fa56fa
danikdesign <danik.newmail@gmail.com>
2022-12-10 20:32:45 +0200

    Update tests (allow a guest to create contact)

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -6,4 +6,15 @@ feature "Contact Creation" do
 
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+
+    fill_in 'contact_email', with: 'email@email.com'
+    fill_in 'contact_message', with: 'Capybara testing message'
+    click_button 'Send message'
+
+    expect(page).to have_content 'Thank you!'
+  end
+
 end
