5f4cbbc300e362a825ee062321474d916f208243
danikdesign <danik.newmail@gmail.com>
2022-12-11 13:43:25 +0200

    Added test to editing an article by user

--- spec/features/user_edits_article_spec.rb
+++ spec/features/user_edits_article_spec.rb
@@ -1,2 +1,22 @@
-# frozen_string_literal: true
+require 'rails_helper'
 
+feature "Editing Article" do
+  before(:each) do
+    sign_up
+    create_article
+  end
+
+  scenario "allow user to edit an article" do
+    visit 'articles/1'
+
+    click_link 'Edit'
+
+    fill_in 'article_title', with: 'Edited title'
+    fill_in 'article_text', with: 'Edited article text'
+
+    click_button 'Send article'
+
+    expect(page).to have_content 'Edited title'
+  end
+
+end
