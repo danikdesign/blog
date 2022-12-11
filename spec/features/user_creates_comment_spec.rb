085e9c1238e776b104dcb9912d51b17b40908903
danikdesign <danik.newmail@gmail.com>
2022-12-11 13:00:59 +0200

    Added test for creating comment by user

--- spec/features/user_creates_comment_spec.rb
+++ spec/features/user_creates_comment_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 feature "Comment Creation" do
   before(:each) do
    sign_up
- end
+   create_article
+  end
 
- scenario "allow user to visit article page with comments" do
-   visit '/articles/1'
-   expect(page).to have_content 'Test title#0123'
- end
+  scenario "allow user to create a comment" do
+    visit '/articles/1'
+
+    fill_in 'comment_body', with: 'Comment for Test title#0123'
+
+    click_button 'Create Comment'
+
+    expect(page).to have_content 'Comment for Test title#0123'
+  end
 
 end
 
